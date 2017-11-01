c57b96896760be768971c42249db9f77b9529864
Luke Rees <escrew7@hotmail.com>
2017-11-02 06:16:53 +0800

    Add tests for PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -86,27 +86,27 @@ app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
-  if(!ObjectID.isValid(id)) {
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
-  if(_.isBoolean(body.completed) && body.completed) {
+  if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime();
   } else {
     body.completed = false;
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {$new: true}).then((todo) => {
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
     if (!todo) {
-      return res.status(404).send()
+      return res.status(404).send();
     }
 
     res.send({todo});
   }).catch((e) => {
-    res.status(400).send()
+    res.status(400).send();
   })
-})
+});
 
 
 
