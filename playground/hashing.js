535d100a9e8f4491d337e51624afbbf0a40be4c0
Luke Rees <escrew7@hotmail.com>
2017-12-26 08:28:33 +0800

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-var data = {
-  id: 3
-}
+var password = '123abc'
 
-var token = jwt.sign(data, '123abc')
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash)
+  })
+})
 
-console.log(token)
+var hashedPassword = '$2a$10$.paWLOJ21c89Ihv5h7SYl.QtFMFD4e2Tz./H/8QKoJLwIg2dKF/.W'
 
-var decoded = jwt.verify(token, '123abc')
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res)
+})
 
-console.log(decoded)
+// var data = {
+//   id: 3
+// }
+//
+// var token = jwt.sign(data, '123abc')
+//
+// console.log(token)
+//
+// var decoded = jwt.verify(token, '123abc')
+//
+// console.log(decoded)
 // var msg = 'I am user #3'
 // var hash = SHA256(msg)
 //
